6c7fd540e9bb628a19b1f19012b70c84bd864c25
Francisco_GP <91037607+PACKYTO23@users.noreply.github.com>
2023-12-01 15:36:46 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# # # Functions get activated by the parentheses.
+
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+
+# # # Defining functions.
+
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+
+# # # Calling functions.
+
+# my_function()
+
+# / # / # EXERCISES # / # / #
+
+# Reeborg's World
+
+# / # / # / # / # / #
+
+# / # / # PROJECT OF DAY 6 # / # / #
+
+# Reeborg's World
